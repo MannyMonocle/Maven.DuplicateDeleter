0121aa7ba200d4bdedd29628c5a9a6c5e4d26ad6
MannyMonocle <eetumadu@gmail.com>
2019-11-16 11:40:55 -0500

    done

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,30 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        return Arrays.stream(array).filter(ele -> countFrequency(ele) < maxNumberOfDuplications).toArray(Integer[] :: new);
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        return Arrays.stream(array).filter(ele -> countFrequency(ele) != exactNumberOfDuplications).toArray(Integer[] :: new);
+    }
+
+    public Long countFrequency(Integer lookingFor){
+        return Arrays.stream(array).filter(element -> element == lookingFor).count();
+    }
+
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,31 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+
+
+
+    public StringDuplicateDeleter(String[] stringArray) {
+        super(stringArray);
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        return Arrays.stream(array).filter(ele -> countFrequency(ele) < maxNumberOfDuplications).toArray(String[] :: new);
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        return Arrays.stream(array).filter(ele -> countFrequency(ele) != exactNumberOfDuplications).toArray(String[] :: new);
+    }
+
+    public Long countFrequency(String lookingFor){
+        return Arrays.stream(array).filter(element -> element == lookingFor).count();
+    }
+
 }
